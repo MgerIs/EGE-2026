824e375d7c4131f24eb2328a8a824cefa0effa78
MIsabekyan <geragswagga@gmail.com>
2025-01-15 17:58:12 +0200

    типы данных

--- 15-01-25/lesson-1.py
+++ 15-01-25/lesson-1.py
@@ -1,2 +1,30 @@
-print('Hello world!')
-print('goodbye')
+var_int = 4  # целое число
+
+var_float = 4.3  # дробное число
+print(type(var_float))  # строка
+
+var_str = 'hello world!'
+print(type(var_str))
+
+var_list = [123, "lol", 2.71]  # список
+
+print(type(var_list))
+
+var_tuple = (3, 14, 'gg', 21)  # кортеж
+
+print(type(var_tuple))
+
+var_set = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}  # мн-во
+
+print(var_set)
+
+var_dict = {'name': 'adolf', 'age': 19, 'funny': False}  # словарь
+
+print(var_dict['name'])
+
+print(var_dict['age'])
+
+
+var_bool = True #логика
+
+
