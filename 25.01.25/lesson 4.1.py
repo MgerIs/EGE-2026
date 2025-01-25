6824d82a10a3dd1c6f50160bbf131a01df4b0fba
MIsabekyan <geragswagga@gmail.com>
2025-01-25 13:46:27 +0200

    циклы

---
+++ 25.01.25/lesson 4.1.py
@@ -0,0 +1,20 @@
+# циклы
+
+
+num1 = int(input())
+while num1 > 0:
+    print("hello")
+    num1 -= 1
+
+num2 = 0
+num3 = int(input())
+while num3 > num2:
+    print("hello")
+    num3 -= 1
+
+
+
+
+
+
+
