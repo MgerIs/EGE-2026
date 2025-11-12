063a26419116c09c651356722bcf5dbebec637e5
MgerIs <geragswagga@gmail.com>
2025-11-12 21:59:08 +0200

    task-5

--- Task-6/Theory.py
+++ Task-6/Theory.py
@@ -1,44 +1,38 @@
- Модуль Turtle
-
+import turtle
 from turtle import *
-
-# Изменение размера экрана
-screensize(3000, 3000)
-# Отключение анимации
+screensize(2000,2000)
+shape("turtle")
+k = 10
 tracer(0)
-# Обновление данных экрана
-update()
-
-# Движение черепахи вперед по направлению головы
-forward(100)
-fd(100)
-
-# Движение черепахи назад по направлению головы
-backward(100)
-bk(100)
-
-# Поворот черепахи вправо по направлению головы на N градусов
-right(90)
-rt(90)
-
-# Поворот черепахи влево по направлению головы на N градусов
+speed(10000000000)
 left(90)
-lt(90)
-
-# Поднять хвост
-penup()
-up()
-
-# Опустить хвост
 pendown()
-down()
+for i in range(2):
+    forward(10*k)
+    right(90)
+    forward(18*k)
+    right(90)
+
+penup()
+for i in range(1):
+    forward(5*k)
+    right(90)
+    forward(7*k)
+    left(90)
+pendown()
+for i in range(2):
+    forward(10 * k)
+    right(90)
+    forward(7 * k)
+    right(90)
+penup()
+for x in range(-100,100):
+    for y in range(-100,100):
+        goto(x * k, y * k)
+        dot(3, "red")
+for x in range(-100,100):
+    for y in range(-100,100):
+        goto(x * k, y * k)
+        dot(3, "red")
 
 done()
-
-# Расстановка точек на плоскости
-up()
-for x in range(-10, 20):
-    for y in range(-20, 10):
-        goto(x * m, y * m)
-        dot(3, 'red')
-
