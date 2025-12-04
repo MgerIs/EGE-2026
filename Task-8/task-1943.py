9fd124dd4d66476a19ca89373c28169c9017f831
MgerIs <geragswagga@gmail.com>
2025-12-04 17:56:51 +0200

    task-8

--- Task-8/task-1943.py
+++ Task-8/task-1943.py
@@ -1,8 +1,8 @@
-from itertools import *
-
+from itertools import product
 cnt = 0
-alph = sorted("ЗЕРКАЛО")
-for val in product(alph, repeat=6):
-    val = "".join(val)
-    if "К" in val and val.count("К")<=4:
-        for i in "ЗЕРКАЛО":
+for x in product('ПИТОН', repeat=4):
+    s = ''.join(x)
+    if 'ИО' not in s and 'ОИ' not in s and 'ОО' not in s and 'ИИ' not in s and 'ПТ' not in s and 'ТП' not in s and 'ТН' not in s \
+        and 'НТ' not in s and 'ПН' not in s and 'НП' not in s and 'ПП' not in s and 'НН' not in s and 'ТТ' not in s:
+        cnt += 1
+print(cnt)
