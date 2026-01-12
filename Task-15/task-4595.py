f5e48927f9e7a3d63ae7041987a023711a24bded
MIsabekyan <geragswagga@gmail.com>
2026-01-12 17:34:22 +0200

    task 15

--- Task-15/task-4595.py
+++ Task-15/task-4595.py
@@ -1,6 +1,6 @@
 def f(A):
     for x in range(1,100):
-            f = ((x%2==0) <= (x%3!=0)) or (x + A >= 80)
+            f = (x%2==0) <= (x%3!=0) or (x + A >= 80)
             if not f:
                 return False
     return True
