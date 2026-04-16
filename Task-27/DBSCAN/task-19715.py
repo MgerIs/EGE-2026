04f8c3ae06e533370185fbb49d200ddec7664564
MIsabekyan <geragswagga@gmail.com>
2026-04-16 17:48:56 +0200

    tasks

--- Task-27/DBSCAN/task-19715.py
+++ Task-27/DBSCAN/task-19715.py
@@ -1,6 +1,7 @@
 from math import *
-#
-#
+
+
+
 def center(cluster):
     res = []
     for dot1 in cluster:
@@ -8,26 +9,26 @@ def center(cluster):
         res.append([sum_dist, dot1])
     return min(res)[1]
 
-#
-# with open("27.21.A_19715.txt") as file:
-#     dots = [list(map(float, i.replace(",", ".").split())) for i in file]
-#
-# eps = 1
-# clusters = []
-# while dots:
-#     cluster = [dots.pop()]
-#     for dot in cluster:
-#         for d in dots.copy():
-#             if dist(dot, d) < eps:
-#                 cluster.append(d)
-#                 dots.remove(d)
-#     if len(cluster) > 30:
-#         clusters.append(cluster)
-# print([len(cluster) for cluster in clusters])
-# centers = [center(cluster) for cluster in clusters]
-# print(centers)
-# print((centers[0][0]+centers[1][0])/2*10000)
-# print((centers[0][1]+centers[1][1])/2*10000)
+
+with open("27.21.A_19715.txt") as file:
+    dots = [list(map(float, i.replace(",", ".").split())) for i in file]
+
+eps = 1
+clusters = []
+while dots:
+    cluster = [dots.pop()]
+    for dot in cluster:
+        for d in dots.copy():
+            if dist(dot, d) < eps:
+                cluster.append(d)
+                dots.remove(d)
+    if len(cluster) > 30:
+        clusters.append(cluster)
+print([len(cluster) for cluster in clusters])
+centers = [center(cluster) for cluster in clusters]
+print(centers)
+print((centers[0][0]+centers[1][0])/2*10000)
+print((centers[0][1]+centers[1][1])/2*10000)
 
 with open("../files/27.21.B_19715.txt") as file:
     dots = [list(map(float, i.replace(",", ".").split())) for i in file]
